036afdfd2cfbd5c20c822a8af16957291eea54b1
Gaurav Narula <gnarula94@gmail.com>
2013-07-10 14:18:49 +0530

    Added index.html

--- web.js
+++ web.js
@@ -2,8 +2,11 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  output = fs.readFileSync('index.html').toString();
+  response.send(output);
 });
 
 var port = process.env.PORT || 5000;
